354af8f89627f49457d0a6abf8019dd4307cd52e
Unknown <berardteddy@icloud.com>
2019-06-24 18:38:20 +0200

    :tada: Add xcode project

---
+++ Travel/Travel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Teddy Bérard on 24/06/2019.
+//  Copyright © 2019 TeddyBerard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
