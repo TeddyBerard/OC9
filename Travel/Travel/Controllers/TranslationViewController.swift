7d7d7d48831fefdc083987ff6f9aaf6a1acbae54
Teddy Berard <berardteddy@icloud.com>
2019-09-09 18:24:54 +0200

    :lipstick: Update some UI content and Add UI translation

--- Travel/Travel/Controllers/TranslationViewController.swift
+++ Travel/Travel/Controllers/TranslationViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class TranslationViewController: UIViewController {
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    
+    // MARK: - Properties
+
+    @IBOutlet weak var wantedTranslatedTextView: UITextView!
+    @IBOutlet weak var translatedTextView: UITextView!
+    @IBOutlet weak var translateButton: UIButton!
+
+
+    // MARK: - Cycle Life
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupButton()
+    }
+
+    func setupButton() {
+        translateButton.layer.borderWidth = 1
+        translateButton.layer.borderColor = UIColor.black.cgColor
+    }
+
+    // MARK: - @IBAction
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    @IBAction func translateAction(_ sender: Any) {
 
     }
 
+    @IBAction func clearAction(_ sender: Any) {
+        wantedTranslatedTextView.text = ""
+    }
+
+}
+
+extension TranslationViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if wantedTranslatedTextView.text == "Entrez un texte à traduire" {
+            wantedTranslatedTextView.text = ""
+        }
+    }
 }
